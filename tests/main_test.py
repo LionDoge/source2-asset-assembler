ce3086f9808d7e361ec9123778cfe17b06bc8064
Lion Doge <KrystianoXPL@gmail.com>
2024-09-17 09:06:25 +0200

    Fix test fixture

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,11 +1,10 @@
 from assetbuilder import readAssetFile, AssetInfo, FileBlock
-import pytest
 
 def file1AssetInfo():
     return AssetInfo(version=2, headerVersion=12, blocks=[
-		FileBlock(type="kv3", name="RED2", data=None),
-		FileBlock(type="text", name="DATA", data=None),
-        FileBlock(type="kv3", name="STAT", data=None)
+		FileBlock(type="kv3", name="RED2", data=None, dataProcessed=False),
+		FileBlock(type="text", name="DATA", data=None, dataProcessed=False),
+        FileBlock(type="kv3", name="STAT", data=None, dataProcessed=False),
 	])
 
 def test_readAssetFile():
