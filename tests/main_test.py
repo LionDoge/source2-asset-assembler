9cd0631170410521bc4182a5357ad36a6d8167a5
Lion Doge <KrystianoXPL@gmail.com>
2024-09-29 19:08:54 +0200

    Specify kv3 version if test fixture

--- tests/main_test.py
+++ tests/main_test.py
@@ -2,9 +2,9 @@ from assetbuilder import readAssetFile, AssetInfo, FileBlock
 
 def file1AssetInfo():
     return AssetInfo(version=2, headerVersion=12, blocks=[
-		FileBlock(type="kv3", name="RED2", data=None, dataProcessed=False),
+		FileBlock(type="kv3v4", name="RED2", data=None, dataProcessed=False),
 		FileBlock(type="text", name="DATA", data=None, dataProcessed=False),
-        FileBlock(type="kv3", name="STAT", data=None, dataProcessed=False),
+        FileBlock(type="kv3v4", name="STAT", data=None, dataProcessed=False),
 	])
 
 def test_readAssetFile():
