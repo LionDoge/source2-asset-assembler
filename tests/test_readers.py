93c993eab59e4fe4cfb1113bbb600b45fe92bd06
Lion Doge <KrystianoXPL@gmail.com>
2024-10-15 01:34:56 +0200

    more tests

--- tests/test_readers.py
+++ tests/test_readers.py
@@ -1,4 +1,5 @@
-from assetbuilder import readAssetFile, AssetInfo, FileBlock
+from assetbuilder import readAssetFile, AssetInfo, FileBlock, readRERLTextFile, FileFormattingError, AssetReadError
+from test_writers import example_rerl_source
 import pytest
 
 @pytest.fixture
@@ -9,5 +10,25 @@ def file1AssetInfo():
         FileBlock(type="kv3v4", name="STAT", data=None, dataProcessed=False),
 	])
 
+@pytest.fixture
+def example_rerl_file():
+    with open("tests/files/rerltest.txt", "r") as f:
+        return f.read()
+
 def test_readAssetFileBlockInfo(file1AssetInfo):
     assert readAssetFile("tests/files/asset1.vts_c") == file1AssetInfo
+
+def test_readRerlData(example_rerl_file, example_rerl_source):
+    assert readRERLTextFile(example_rerl_file) == example_rerl_source
+
+def test_rerlTextRaisesErrorWithInvalidNumber():
+    with pytest.raises(FileFormattingError):
+        readRERLTextFile("1000a something")
+
+def test_rerlTextRaisesErrorWithNegativeNumber():
+    with pytest.raises(FileFormattingError):
+        readRERLTextFile("-1 something")
+
+def test_rerlTextRaisesErrorWithInvalidLineFormat():
+    with pytest.raises(FileFormattingError):
+        readRERLTextFile("0")
